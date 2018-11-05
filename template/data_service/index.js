b32630a2fac4edc37639c968064f295ef1136af0
BjarkiHrafn <bjarki15@ru.is>
2018-11-05 14:09:01 +0000

    tiny change

---
+++ template/data_service/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/planets', (res, req) => {
+    return 'ye';
+})
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
